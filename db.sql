1f3a8e6c09173eb681160161911b564ea5b421c4
Jhovany Morales Bruno <jhovanymb.info@gmail.com>
2019-11-27 05:02:47 +0000

    db initialized

--- db.sql
+++ db.sql
@@ -1,1 +1,12 @@
-CREATE TABLE
+-- CREATE SCHEMA Seguridad;
+
+-- CREATE SEQUENCE Seguridad.Usuario_Id_Usuario;
+
+CREATE TABLE IF NOT EXISTS Seguridad.Usuario(
+Id_Usuario SERIAL PRIMARY KEY,
+Nombre_Usuario VARCHAR(50) UNIQUE NOT NULL,
+Password_Usuario VARCHAR (300) NOT NULL,
+Salt_Usuario VARCHAR (50));
+ 
+ ALTER SEQUENCE Seguridad.Usuario_Id_Usuario
+ OWNED BY Seguridad.Usuario;
